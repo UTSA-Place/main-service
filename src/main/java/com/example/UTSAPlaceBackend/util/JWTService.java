0f5608e6c2e182233daa351d21a4dc01a33234cf
Chucksvill3 <102399735+Chucksvill3@users.noreply.github.com>
2025-02-26 16:26:08 -0600

    updated depracated method

--- src/main/java/com/example/UTSAPlaceBackend/util/JWTService.java
+++ src/main/java/com/example/UTSAPlaceBackend/util/JWTService.java
@@ -1,12 +1,12 @@
 package com.example.UTSAPlaceBackend.util;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,16 +14,17 @@ import java.util.function.Function;
 
 @Component
 public class JWTService {
-    private String secret = "UTSA";
+    private Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
     public String createToken(String username) {
         Map<String, Object> claims = new HashMap<>();
-        return Jwts.builder()
-                .setClaims(claims)
-                .setSubject(username)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7)) // 1 week
-                .signWith(SignatureAlgorithm.HS256, secret)
+        return Jwts
+                .builder()
+                .claims(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7)) // 1 week
+                .signWith(secretKey)
                 .compact();
     }
 
@@ -33,7 +34,7 @@ public class JWTService {
     }
 
     public String extractUsername(String tokenString) {
-        return extractClaim(tokenString, Claims::getSubject);       
+        return extractClaim(tokenString, Claims::getSubject);
     }
 
     private Date extractExpiration(String token) {
@@ -46,7 +47,7 @@ public class JWTService {
     }
 
     private Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+        return Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody();
     }
 
     private Boolean isTokenExpired(String token) {
