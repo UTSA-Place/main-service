334fce94c33111d8273fdf05c8c2fff72aa015b7
Chucksvill3 <102399735+Chucksvill3@users.noreply.github.com>
2025-03-13 14:16:09 -0500

    email

--- src/main/java/com/example/UTSAPlaceBackend/util/EmailValidator.java
+++ src/main/java/com/example/UTSAPlaceBackend/util/EmailValidator.java
@@ -1,14 +1,21 @@
 package com.example.UTSAPlaceBackend.util;
 
-import org.springframework.stereotype.Component;
 import java.util.function.Predicate;
+
+import org.springframework.stereotype.Component;
 
 @Component
 public class EmailValidator implements Predicate<String> {
+    private final String UTSA_EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@utsa.edu$";
+    private final String UTSA_EMAIL_REGEX_2 = "^[a-zA-Z0-9._%+-]+@my.utsa.edu$";
 
     @Override
     public boolean test(String s) {
-        // TODO: regex to validate as UTSA email
-        return true;
+        // Check if the email matches the regex for UTSA email
+        if (s == null || s.isEmpty()) {
+            return false;
+        }
+        // Check if the email matches the regex for UTSA email
+        return s.matches(UTSA_EMAIL_REGEX) || s.matches(UTSA_EMAIL_REGEX_2);
     }
 }
